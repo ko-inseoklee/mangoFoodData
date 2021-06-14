14938f081c32d046a84cc84f3f733a8687125e32
ko-inseoklee <60339952+ko-inseoklee@users.noreply.github.com>
2021-06-14 20:57:36 +0900

    add how to upload data on firebase

---
+++ config.js
@@ -0,0 +1,11 @@
+import { appKey } from "./appkey";
+
+var firebaseConfig = {
+    apiKey: appKey,
+    authDomain: "testios-3060d.firebaseapp.com",
+    projectId: "testios-3060d",
+    storageBucket: "testios-3060d.appspot.com",
+    messagingSenderId: "868564849053",
+    appId: "1:868564849053:web:6cb90a0e877d82c847df74",
+    measurementId: "G-95D9NTSPWF"
+};
---
+++ import.js
@@ -0,0 +1,19 @@
+const firestoreService = require('firestore-export-import');
+const firebaseConfig = require('./config.js');
+const serviceAccount = require('./serviceAccount.json');
+
+const jsonToFirestore = async() => {
+    try{
+        console.log('Initializing Firebase');
+        await firestoreService.initializeApp(serviceAccount, firebaseConfig.databaseURL);
+        console.log('Firebase Initialized');
+
+        
+        await firestoreService.restore('./foodData2.json');
+        console.log("upload suceess");
+    } catch (error){
+        console.log(error);
+    }
+};
+
+jsonToFirestore();
